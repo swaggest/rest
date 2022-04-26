4facbfcc2d097e55d54f59ab5d18c8c6bf06e8de
Viacheslav Poturaev <vearutop@gmail.com>
2022-04-26 18:17:30 +0200

    Add example of slice types documentation

--- _examples/advanced/json_map_body.go
+++ _examples/advanced/json_map_body.go
@@ -3,9 +3,20 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"net"
 
+	"github.com/google/uuid"
+	"github.com/swaggest/jsonschema-go"
 	"github.com/swaggest/usecase"
 )
+
+type URL string
+
+func (URL) PrepareJSONSchema(schema *jsonschema.Schema) error {
+	schema.WithFormat("uri")
+
+	return nil
+}
 
 type JSONMapPayload map[string]float64
 
@@ -19,13 +30,25 @@ func (j *jsonMapReq) UnmarshalJSON(data []byte) error {
 	return json.Unmarshal(data, &j.JSONMapPayload)
 }
 
-func jsonMapBody() usecase.Interactor {
-	type jsonOutput struct {
-		Header string         `json:"inHeader"`
-		Query  int            `json:"inQuery"`
-		Data   JSONMapPayload `json:"data"`
-	}
+type jsonOutput struct {
+	Header string         `json:"inHeader"`
+	Query  int            `json:"inQuery"`
+	Data   JSONMapPayload `json:"data"`
+	UUIDs  []uuid.UUID    `json:"uuids"` // Schema provided via type mapping globally.
+	IPs    []net.IP       `json:"ips"`   // Schema provided via type mapping globally.
+	URLs1  []URL          `json:"urls1"` // Schema prepared for named type.
+	URLs2  []string       `json:"urls2"` // Schema updated in parent, no named type needed.
+}
 
+func (jsonOutput) PrepareJSONSchema(schema *jsonschema.Schema) error {
+	prop := schema.Properties["urls2"]
+	items := prop.TypeObjectEns().ItemsEns()
+	items.SchemaOrBoolEns().TypeObjectEns().WithFormat("uri").WithMinLength(5)
+
+	return nil
+}
+
+func jsonMapBody() usecase.Interactor {
 	u := usecase.NewIOI(new(jsonMapReq), new(jsonOutput), func(ctx context.Context, input, output interface{}) (err error) {
 		var (
 			in  = input.(*jsonMapReq)
--- _examples/go.mod
+++ _examples/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/bool64/httpmock v0.1.1
 	github.com/bool64/httptestbench v0.1.3
 	github.com/go-chi/chi/v5 v5.0.7
+	github.com/google/uuid v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/stretchr/testify v1.7.1
 	github.com/swaggest/assertjson v1.6.8
