0f5ebfc3b8ffd53b8d7df3c6caae8c8d45b7feea
Viacheslav Poturaev <vearutop@gmail.com>
2022-07-22 12:23:01 +0200

    Inspect wrapped handlers in Mount (#86)

--- _examples/go.mod
+++ _examples/go.mod
@@ -6,13 +6,13 @@ replace github.com/swaggest/rest => ../
 
 require (
 	github.com/bool64/ctxd v1.1.2
-	github.com/bool64/dev v0.2.16
-	github.com/bool64/httpmock v0.1.1
+	github.com/bool64/dev v0.2.18
+	github.com/bool64/httpmock v0.1.6
 	github.com/bool64/httptestbench v0.1.3
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/rs/cors v1.8.2
-	github.com/stretchr/testify v1.7.2
+	github.com/stretchr/testify v1.8.0
 	github.com/swaggest/assertjson v1.7.0
 	github.com/swaggest/jsonschema-go v0.3.35
 	github.com/swaggest/openapi-go v0.2.18
@@ -25,7 +25,7 @@ require github.com/swaggest/rest v0.0.0-00010101000000-000000000000
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/bool64/shared v0.1.4 // indirect
+	github.com/bool64/shared v0.1.5 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
@@ -41,8 +41,6 @@ require (
 	github.com/yosuke-furukawa/json5 v0.1.2-0.20201207051438-cf7bb3f354ff // indirect
 	github.com/yudai/gojsondiff v1.0.0 // indirect
 	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ _examples/mount/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/swaggest/rest/nethttp"
+	"github.com/swaggest/rest/web"
+	swgui "github.com/swaggest/swgui/v4emb"
+	"github.com/swaggest/usecase"
+)
+
+func mul() usecase.Interactor {
+	return usecase.NewInteractor(func(ctx context.Context, input []int, output *int) error {
+		*output = 1
+
+		for _, v := range input {
+			*output *= v
+		}
+
+		return nil
+	})
+}
+
+func sum() usecase.Interactor {
+	return usecase.NewInteractor(func(ctx context.Context, input []int, output *int) error {
+		for _, v := range input {
+			*output += v
+		}
+
+		return nil
+	})
+}
+
+func main() {
+	service := web.DefaultService()
+	service.OpenAPI.Info.Title = "Security and Mount Example"
+
+	apiV1 := web.DefaultService()
+
+	apiV1.Wrap(
+		middleware.BasicAuth("Admin Access", map[string]string{"admin": "admin"}),
+		nethttp.HTTPBasicSecurityMiddleware(service.OpenAPICollector, "Admin", "Admin access"),
+	)
+
+	apiV1.Post("/sum", sum())
+	apiV1.Post("/mul", mul())
+
+	service.Mount("/api/v1", apiV1)
+	service.Docs("/api/v1/docs", swgui.New)
+
+	// Blanket handler, for example to serve static content.
+	service.Mount("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("blanket handler got a request: " + r.URL.String()))
+	}))
+
+	fmt.Println("Swagger UI at http://localhost:8010/api/v1/docs.")
+	if err := http.ListenAndServe("localhost:8010", service); err != nil {
+		log.Fatal(err)
+	}
+}
