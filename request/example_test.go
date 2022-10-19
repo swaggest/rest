adf4d98b76a5282f5767ed2d41ba07af1f1e166d
Cuss Rox <42931691+cussrox@users.noreply.github.com>
2022-10-19 20:02:41 +0200

    Request decoder standalone example (#92)

---
+++ request/example_test.go
@@ -0,0 +1,31 @@
+package request_test
+
+import (
+	"net/http"
+
+	"github.com/swaggest/rest/request"
+)
+
+func ExampleDecoder_Decode() {
+	type MyRequest struct {
+		Foo int    `header:"X-Foo"`
+		Bar string `formData:"bar"`
+		Baz bool   `query:"baz"`
+	}
+
+	// A decoder for particular structure, can be reused for multiple HTTP requests.
+	myDecoder := request.NewDecoderFactory().MakeDecoder(http.MethodPost, new(MyRequest), nil)
+
+	// Request and response writer from ServeHTTP.
+	var (
+		rw  http.ResponseWriter
+		req *http.Request
+	)
+
+	// This code would presumably live in ServeHTTP.
+	var myReq MyRequest
+
+	if err := myDecoder.Decode(req, &myReq, nil); err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+	}
+}
