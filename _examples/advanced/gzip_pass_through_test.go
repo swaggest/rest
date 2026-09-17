0db2ac4fbb11195671730a861828c245ead29692
Viacheslav Poturaev <vearutop@gmail.com>
2026-09-17 23:18:36 +0200

    Fix test

--- _examples/advanced/gzip_pass_through_test.go
+++ _examples/advanced/gzip_pass_through_test.go
@@ -75,7 +75,7 @@ func Test_directGzip_perf(t *testing.T) {
 	res := testing.Benchmark(Benchmark_directGzip)
 
 	if httptestbench.RaceDetectorEnabled {
-		assert.Less(t, res.Extra["B:rcvd/op"], 660.0)
+		assert.Less(t, res.Extra["B:rcvd/op"], 670.0)
 		assert.Less(t, res.Extra["B:sent/op"], 104.0)
 		assert.Less(t, res.AllocsPerOp(), int64(62))
 		assert.Less(t, res.AllocedBytesPerOp(), int64(9000))
